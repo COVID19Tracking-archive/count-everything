eefe848b2fdd9a0487b04421ec218cc80c49b8f7
Kevin Miller <keveemiller@gmail.com>
2021-02-26 14:48:49 -0800

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## The COVID Tracking Project Track Everything
+
+A series of tools to count up total amounts of data in our datasets.
+
+To run:
+
+```
+yarn run checkout
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const files = [
+  "./covid-public-api/v1/us/daily.json",
+  "./covid-public-api/v1/states/daily.json",
+  "./website-data/annotations.json",
+  "./website-data/crdt_api.json",
+  "./website-data/crdt_timeseries.json",
+  "./website-data/crdt_test_hosp_race_separate.json",
+  "./website-data/crdt_test_hosp_race_combined.json",
+  "./website-data/crdt_states_combined.json",
+  "./website-data/crdt_states_separate.json",
+  "./website-data/crdt_sources.json",
+  "./website-data/grades_state_assessment.json",
+  "./website-data/long_term_care_facilities.json",
+  "./website-data/long_term_care_states.json",
+  "./website-data/screenshots.json",
+];
+
+let total = 0;
+files.forEach((file) => {
+  const rows = JSON.parse(fs.readFileSync(file).toString());
+  total += rows.length * Object.keys(rows[0]).length;
+});
+
+console.log(`${total} datapoints from ${files.length} files.`);
